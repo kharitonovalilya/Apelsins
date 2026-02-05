29348435c3dd984e7e8cf641c2f58998fc1f3874
Goncharova Ekaterina <ekkigoncharova@edu.hse.ru>
2026-02-05 18:50:30 +0300

    add step function

--- seminar-3/include/logic.h
+++ seminar-3/include/logic.h
@@ -1,3 +1,6 @@
 #include <vector>
 
 int countInfected(const std::vector<std::vector<int>> &grid);
+bool neighbour_vir(const std::vector<std::vector<int>> &grid, int pos_x,
+                   int pos_y);
+std::vector<std::vector<int>> step(const std::vector<std::vector<int>> &grid);
--- seminar-3/src/logic.cpp
+++ seminar-3/src/logic.cpp
@@ -9,3 +9,40 @@ int countInfected(const std::vector<std::vector<int>> &grid) {
   }
   return result;
 }
+
+bool neighbour_vir(const std::vector<std::vector<int>> &grid, int pos_x,
+  int pos_y) {
+int x_size = static_cast<int>(grid.size());
+int y_size = static_cast<int>(grid[0].size());
+if (pos_x + 1 < x_size && grid[pos_x + 1][pos_y] == 1) {
+return true;
+}
+if (pos_x - 1 >= 0 && grid[pos_x - 1][pos_y] == 1) {
+return true;
+}
+if (pos_y + 1 < y_size && grid[pos_x][pos_y + 1] == 1) {
+return true;
+}
+if (pos_y - 1 >= 0 && grid[pos_x][pos_y - 1] == 1) {
+return true;
+}
+return false;
+}
+
+std::vector<std::vector<int>> step(const std::vector<std::vector<int>> &grid) {
+int x_size = static_cast<int>(grid.size());
+int y_size = static_cast<int>(grid[0].size());
+std::vector<std::vector<int>> new_grid = grid;
+for (int pos_x = 0; pos_x < x_size; pos_x++) {
+for (int pos_y = 0; pos_y < y_size; pos_y++) {
+if (grid[pos_x][pos_y] == 1) {
+new_grid[pos_x][pos_y] = 1;
+} else if (neighbour_vir(grid, pos_x, pos_y)) {
+new_grid[pos_x][pos_y] = 1;
+} else {
+new_grid[pos_x][pos_y] = 0;
+}
+}
+}
+return new_grid;
+}
