e164ad103fdcb81db6a5070bef1c09c92b060539
Goncharova Ekaterina <ekkigoncharova@edu.hse.ru>
2026-02-05 19:15:41 +0300

    add tests neighbour_vir step

---
+++ seminar-3/tests/test_neighbour_vir.cpp
@@ -0,0 +1,15 @@
+#include <logic.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Check heighbours if they are viruses") {
+  SECTION("Check neighbours") {
+    std::vector<std::vector<int>> grid = {{1, 0, 1}, {0, 0, 1}, {0, 0, 1}};
+    std::vector<std::vector<int>> simple_grid = {{1}};
+    REQUIRE(neighbour_vir(grid, 0, 0) == true);
+    REQUIRE(neighbour_vir(grid, 1, 1) == true);
+    REQUIRE(neighbour_vir(grid, 0, 2) == false);
+    REQUIRE(neighbour_vir(grid, 0, 2) == false)
+    REQUIRE(neighbour_vir(simple_grid, 0, 0) == false);
+  }
+}
---
+++ seminar-3/tests/test_step.cpp
@@ -0,0 +1,16 @@
+#include <logic.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Check one step of viruses spread on board") {
+  SECTION("Step") {
+    std::vector<std::vector<int>> grid1 = {{1, 0, 1}, {0, 0, 1}, {0, 0, 1}};
+    std::vector<std::vector<int>> grid1_new = {{1, 1, 1}, {1, 0, 1}, {0, 1, 1}};
+    REQUIRE(step(grid1) == grid1_new);
+    std::vector<std::vector<int>> grid2 = {
+        {1, 0, 1, 0}, {0, 0, 0, 0}, {0, 0, 1, 0}};
+    std::vector<std::vector<int>> grid2_new = {
+        {1, 1, 1, 1}, {1, 0, 1, 0}, {0, 1, 1, 1}};
+    REQUIRE(step(grid2) == grid2_new);
+  }
+}
