0f16538fa5ebd3a70edc7ad7d995b8d640cf1ab4
Goncharova Ekaterina <ekkigoncharova@edu.hse.ru>
2026-02-12 15:45:25 +0300

    correct tests sintax mistakes

--- seminar-3/tests/test_step.cpp
+++ seminar-3/tests/test_step.cpp
@@ -4,8 +4,14 @@
 
 TEST_CASE("Check one step of viruses spread on board") {
   SECTION("Step") {
-    std::vector<std::vector<int>> grid1 = {{1, 0, 1}, {0, 0, 1}, {0, 0, 1}};
-    std::vector<std::vector<int>> grid1_new = {{1, 1, 1}, {1, 0, 1}, {0, 1, 1}};
+    std::vector<std::vector<int>> grid1 = {
+      {1, 0, 1}, 
+      {0, 0, 1}, 
+      {0, 0, 1}};
+    std::vector<std::vector<int>> grid1_new = {
+      {1, 1, 1}, 
+      {1, 1, 1}, 
+      {0, 1, 1}};
     REQUIRE(step(grid1) == grid1_new);
     std::vector<std::vector<int>> grid2 = {
         {1, 0, 1, 0}, {0, 0, 0, 0}, {0, 0, 1, 0}};
