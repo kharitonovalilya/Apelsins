c8c47c3f1b25704c928d6752815366469823483c
Batyshkina Alena <aibatyshkina@edu.hse.ru>
2026-02-19 14:51:56 +0300

    Added header for writing function

---
+++ seminar-4/include/write_statistics_csv.h
@@ -0,0 +1,12 @@
+#ifndef WRITE_STATISTICS_CSV_H
+#define WRITE_STATISTICS_CSV_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+void write_statistics_csv(const std::string& filename,
+                          const std::map<std::string, std::vector<double>>& data,
+                          char delimiter = ',');
+
+#endif
