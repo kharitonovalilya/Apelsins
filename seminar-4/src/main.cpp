a58a7d27d4c49fbf25628fecaff73fb0a26c92cc
Ksenia Busygina <kssebusygina@edu.hse.ru>
2026-02-19 19:10:03 +0300

    update main.cpp

--- seminar-4/src/main.cpp
+++ seminar-4/src/main.cpp
@@ -7,6 +7,6 @@ using TableNumbers = std::map<std::string, std::vector<double>>;
 
 int main() {
   TableNumbers table_numbers = read_and_parse("data/input.csv", ',');
-  write_statistics_csv("../data/output.csv", table_numbers);
+  write_statistics_csv("data/output.csv", table_numbers, ',');
   return 0;
 }
