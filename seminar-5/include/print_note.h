755caf945dabee70ad6ec056fc8a12ae40471dba
Lilija Kharitonova <ligekharitonova@edu.hse.ru>
2026-03-05 15:45:37 +0300

    change main

--- seminar-5/include/print_note.h
+++ seminar-5/include/print_note.h
@@ -1,10 +1,11 @@
 
 #ifndef PRINT_NOTE_H
 #define PRINT_NOTE_H
+#include <iostream>
 #include <stack>
 #include <string>
 #include <unordered_map>
 #include <vector>
 using Note = std::tuple<int, std::string, std::vector<std::string>>;
-void printNote(Note& note);
+void printNote(Note &note);
 #endif
