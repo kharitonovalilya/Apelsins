7f701cabcc30c90dbf8dfcda5e9c66fe063f3c66
Lilija Kharitonova <ligekharitonova@edu.hse.ru>
2026-03-05 15:40:59 +0300

    change headers

--- seminar-5/include/notes.h
+++ seminar-5/include/notes.h
@@ -7,10 +7,10 @@
 using Note = std::tuple<int, std::string, std::vector<std::string>>;
 void addNote(std::vector<Note> &notes,
              std::unordered_map<std::string, std::vector<int>> &tagIndex,
-             std::stack<int> &deletedIds, const std::string &text,
+             std::stack<int> &deletedIds, int nextId, const std::string &text,
              const std::vector<std::string> &tags);
-void deleteNote(std::vector<std::tuple<int, std::string, std::vector<std::string>>>& notes,
-                std::unordered_map<std::string, std::vector<int>>& tagIndex,
-                std::stack<int>& deletedIds,
-                int id);
+void deleteNote(
+    std::vector<std::tuple<int, std::string, std::vector<std::string>>> &notes,
+    std::unordered_map<std::string, std::vector<int>> &tagIndex,
+    std::stack<int> &deletedIds, int id);
 #endif
