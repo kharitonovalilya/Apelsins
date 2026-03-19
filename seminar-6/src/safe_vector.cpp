8928a4bb0b9b47999bfae093701b4755425273fc
Ksenia Busygina <kssebusygina@edu.hse.ru>
2026-03-19 16:56:06 +0300

    update comparator

--- seminar-6/src/safe_vector.cpp
+++ seminar-6/src/safe_vector.cpp
@@ -12,7 +12,7 @@ template <typename T>
 void SafeVector<T>::push_back(const T& value) {
   if (size_ >= capacity_) {
     size_t new_capacity = 1;
-    if (capasity_ != 0) {
+    if (capacity_ != 0) {
       new_capacity = capacity_ * 2;
     }
     reallocate(new_capacity);
