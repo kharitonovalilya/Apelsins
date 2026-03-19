a4c2b2a0fb4afea0957982c8ad587cdc3cae9b87
Ksenia Busygina <kssebusygina@edu.hse.ru>
2026-03-19 17:35:04 +0300

    update cmake and safe_vector; delete not used file

--- seminar-6/include/safe_vector.h
+++ seminar-6/include/safe_vector.h
@@ -1,43 +1,86 @@
-#ifndef SAFEVECTOR_H
-#define SAFEVECTOR_H
-
-#include <cstddef>
+#pragma once
 #include <stdexcept>
 
-template <typename T>
-class SafeVector {
-public:
-    SafeVector();
-    ~SafeVector();
-    using value_type = T;
-    using iterator = T*;
-    using const_iterator = const T*;
-
-    SafeVector(const SafeVector&);
-    SafeVector& operator=(SafeVector other);
-    void push_back(const T& value);
-
-    T& at(size_t index);
-    const T& at(size_t index) const;
-
-    T& operator[](size_t index);
-    const T& operator[](size_t index) const;
-    void swap(SafeVector& other) noexcept;
-    size_t size() const;
-    iterator begin() noexcept { return data_; }
-    iterator end() noexcept { return data_ + size_; }
-
-    const_iterator begin() const noexcept { return data_; }
-    const_iterator end() const noexcept { return data_ + size_; }
-
-    const_iterator cbegin() const noexcept { return data_; }
-    const_iterator cend() const noexcept { return data_ + size_; }
-
+template <typename T> class SafeVector {
 private:
-    T* data_;
-    size_t size_;
-    size_t capacity_;
-    void reallocate(size_t new_capacity);
-};
+  T *data_;
+  size_t size_;
+  size_t capacity_;
 
-#endif
+public:
+  SafeVector() : data_(nullptr), size_(0), capacity_(0) {}
+
+  explicit SafeVector(size_t count) : size_(count), capacity_(count) {
+    data_ = new T[count];
+  }
+
+  SafeVector(const SafeVector &other)
+      : size_(other.size_), capacity_(other.capacity_) {
+    data_ = new T[capacity_];
+    for (size_t i = 0; i < size_; ++i) {
+      data_[i] = other.data_[i];
+    }
+  }
+
+  ~SafeVector() { delete[] data_; }
+
+  SafeVector &operator=(const SafeVector &other) {
+    if (this != &other) {
+      delete[] data_;
+      size_ = other.size_;
+      capacity_ = other.capacity_;
+      data_ = new T[capacity_];
+      for (size_t i = 0; i < size_; ++i) {
+        data_[i] = other.data_[i];
+      }
+    }
+    return *this;
+  }
+
+  T &operator[](size_t index) { return data_[index]; }
+
+  const T &operator[](size_t index) const { return data_[index]; }
+
+  T &at(size_t index) {
+    if (index >= size_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+  }
+
+  const T &at(size_t index) const {
+    if (index >= size_) {
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+  }
+
+  void push_back(const T &value) {
+    if (size_ >= capacity_) {
+      reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+    }
+    data_[size_++] = value;
+  }
+
+  void reserve(size_t new_capacity) {
+    if (new_capacity <= capacity_)
+      return;
+
+    T *new_data = new T[new_capacity];
+    for (size_t i = 0; i < size_; ++i) {
+      new_data[i] = data_[i];
+    }
+    delete[] data_;
+    data_ = new_data;
+    capacity_ = new_capacity;
+  }
+
+  T *begin() noexcept { return data_; }
+  T *end() noexcept { return data_ + size_; }
+
+  const T *begin() const noexcept { return data_; }
+  const T *end() const noexcept { return data_ + size_; }
+
+  size_t size() const { return size_; }
+  bool empty() const { return size_ == 0; }
+};
